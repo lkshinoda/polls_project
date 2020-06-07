3bf165867864795b2c8831f1c69fd8ba3592da46
Eugene <164902@gmail.com>
2020-06-07 15:06:52 +0300

    add models

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.TextField(max_length=2000)
+    true_answer = models.CharField(max_length=255)
+    is_active = models.BooleanField()
+    admin_comment = models.CharField(max_length=255)
+    option_a = models.CharField(max_length=50)
+    option_b = models.CharField(max_length=50)
+    option_c = models.CharField(max_length=50)
+    option_d = models.CharField(max_length=50)
+
+
+class Test(models.Model):
+    question = models.ManyToManyField(Question)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()  # default length = 50
+    overview = models.TextField(max_length=2000)
+    is_active = models.BooleanField()
+    admin_comment = models.CharField(max_length=255)
+
+
+class Poll(models.Model):
+    test = models.ManyToManyField(Test)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    overview = models.TextField(max_length=2000)
+    is_active = models.BooleanField()
+    admin_comment = models.CharField(max_length=255)
